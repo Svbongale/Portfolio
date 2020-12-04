6a81e0b18c2f42b0ee106750f518e6d8d01570b2
Savya Sachi V B <savyanio@gmail.com>
2020-12-04 22:08:18 +0530

    Minor animation changes

--- animation.js
+++ animation.js
@@ -12,7 +12,7 @@ const splash = document.querySelector(".splash");
 document.addEventListener("DOMContentLoaded", () => {
   setTimeout(() => {
       splash.classList.add('display-none');
-  }, 2000);
+  }, 1800);
 });
 
 
